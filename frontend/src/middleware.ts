ae4384e362495364ba3cb3fdbbc048a5b03082b0
Krishnanand N <krishnanand.n@experionglobal.local>
2025-01-24 17:21:03 +0530

    no major changes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 interface DecodedToken {
     sub: string;
