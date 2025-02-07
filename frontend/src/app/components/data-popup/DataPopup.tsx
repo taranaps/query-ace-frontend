5751fc54a2c9c0cb1bb0f71a899d80334acfe327
Muhammed Diyoof <muhammed.diyoof@experionglobal.local>
2025-02-07 12:45:43 +0530

    import validation

--- frontend/src/app/components/data-popup/DataPopup.tsx
+++ frontend/src/app/components/data-popup/DataPopup.tsx
@@ -241,7 +241,6 @@ const DataPopup = ({
    */
   // const handleRemoveTag = (index: number) => handleChange("tags", formData.tags.filter((data, i) => i !== index));
   const handleRemoveTag = (index: number) =>{
-git
   };
   return (
     <div className={styles.popupOverlay}>
