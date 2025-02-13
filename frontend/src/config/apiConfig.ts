12d79c7d0755781b7a34adae537d1f9305143d39
diuuuuuu <diyoof.exp@gmail.com>
2025-02-13 17:31:16 +0530

    Update apiConfig.ts

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://query-ace-backend.onrender.com/api/v1/queryapplication";
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1/queryapplication";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://query-ace-backend.onrender.com/api/v1/queryapplication";
+//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1/queryapplication";
